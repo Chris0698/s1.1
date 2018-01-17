5524ba4a3f3fba152d87744bda3dc3adcce020d7
Chris0698 <chris.aston98@outlook.com>
2018-01-17 10:12:56 +0000

    Seminar changes LED function use

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,52 @@
 #include <mbed.h>
 
-int main() {
+void on(DigitalOut color)
+{
+   color.write(0);
+   wait(0.5);
+}
+
+void off(DigitalOut color)
+{
+    color.write(1);
+    wait(0.5);
+}
+
+int main()
+{
 
     DigitalOut red(PTB22);
+    DigitalOut blue(PTB21);
+    DigitalOut green(PTE26);
 
-    while(1) {
+    while(1)
+    {
+      /*
       red.write(0);
       wait(0.5);
       red.write(1);
       wait(0.5);
-    }
+
+      blue.write(0);
+      wait(0.5);
+      blue.write(1);
+      wait(0.5);
+
+      green.write(0);   //on
+      wait(0.5);
+      green.write(1);   //off
+      wait(0.5);
+      */
+
+      on(red);
+      off(red);
+
+      on(blue);
+      off(blue);
+
+      on(green);
+      off(green);
+     }
+
+
 }
